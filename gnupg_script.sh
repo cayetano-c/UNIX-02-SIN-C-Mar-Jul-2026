dce531d190a99676373439d23f0f49945b768902
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-23 14:50:04 +0000

    EXPORT COMMAND

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,2 +1,3 @@
 gpg --list-key s #This command lists the key.
 gpg --list-secret-keys --keyid-format=long #This command lists the private key.
+gpg --armor --export-secret-keys D83BBF4AAE393973 #This command exports the private key
