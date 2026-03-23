ee56932c5ad414aee9d15df5962aeedcac4fd683
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-23 15:10:30 +0000

    EXPORT MY PARTNER KEY

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,6 @@
 gpg --list-key s #This command lists the key.
 gpg --list-secret-keys --keyid-format=long #This command lists the private key.
 gpg --armor --export-secret-keys D83BBF4AAE393973 #This command exports the private key
+gpg --import Darwin #Public key export
+gpg -armor --export caye.cordova007@gmail.com > mi_clave_publica.asc #Export your public key to a file
+gpg --import Darwin_llave.asc #
