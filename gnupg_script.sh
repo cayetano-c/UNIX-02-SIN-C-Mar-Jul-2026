0bd5e77e0a32e465cef6eb022d11b7acaaf39c5d
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-26 19:53:57 +0000

    FINAL REVIEW OF COMMAND EXPLANATION

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -16,3 +16,4 @@ gpg --edit-key "NOMBRE" #Allows you to manage, modify, and adjust the trust leve
 gpg --sign-key "NOMBRE" #Digitally sign someone else's public key in your GPG keyring.
 gpg --encrypt --sign #It is used to encrypt and digitally sign a file in a single step
 gpg --decrypt final_cifrado_firmadoDarwinAl.txt.gpg #Decrypt and verify a file that has been both encrypted and signed.
+cat  final_cifrado_firmadoDarwinAl.txt.gpg #Show the content of the decrypted file.
