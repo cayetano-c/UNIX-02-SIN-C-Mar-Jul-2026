bbe1f948d05630f0bd49d81fc56731f46390a384
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-27 14:43:24 +0000

    Access to procfs

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -1,2 +1,3 @@
 cd /dev #Accede al directorio de archivos que representa (debian).
 ls #lista el contenido (archivos y carpetas) del directorio en el que te encuentras.
+cd /proc #Accede al directorio de archivos que representa (procfs).
