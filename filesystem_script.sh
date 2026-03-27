0e589bfc700b7f87fd981ec407465a23214b2760
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-27 15:40:48 +0000

    Binary file move and show content

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -3,7 +3,14 @@ ls #List files and directories.
 cd /proc #Change directory to proc directory.
 ls #List process on proc directory.
 cat cpuinfo #Shows CPU information
-cd /
-ls 
-cd /bin
-ls 
+cd / #Change to general directory
+ls #list general directory files
+cd /bin #change to the bin directory
+ls #List bin directory files
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 #Change to the workspace directory
+gcc saludo.c -o saludo_bin #Change the file to a binary file
+sudo mv saludo_bin /bin #Move the file to the bin directory
+ls #List the bin directory to look for the saludo_bin file
+cat saludo_bin #Shows the saludo_bin content
+cd /home #Change directory to Home
+saludo_bin #Print the original file content, not the binary 
