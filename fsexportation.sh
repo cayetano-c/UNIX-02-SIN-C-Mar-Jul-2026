71e4fd9ec6d877184d180c5acf65442365fddd3c
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-30 15:29:17 +0000

    We are inspecting the disk partitions, file systems, and mount points to manage your storage structure.

--- fsexportation.sh
+++ fsexportation.sh
@@ -1,3 +1,8 @@
-sudo apt update #Actualiza la lista de paquetes disponibles y sus versiones desde los repositorios.
-sudo apt upgrade #Instala las versiones más recientes de los programas que ya tienes instalados.
+sudo apt update #Update the list of available packages and their versions from the repositories.
+sudo apt upgrade #Install the latest versions of the programs you already have installed.
 sudo apt install parted #Download and install Parted, a tool for managing hard drive partitions.
+pip install numpy #Install the NumPy library to perform numerical calculations and efficiently handle multidimensional arrays.
+sudo parted -l && echo -e "\---\n" && lsblk -f && echo -e "\n---\n"
+sudo parted -l #Tells you the physical size of the disk, the type of partition table (GPT or MBR) and whether there is unallocated free space.
+lsblk -f #It is key to identifying which partition corresponds to which folder and what format they have.
+
