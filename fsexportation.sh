5a5ef5366f8a85f1089325385b4ae02d91c7bc7d
cayetano-c <cacordovaro@uide.edu.ec>
2026-03-30 15:17:25 +0000

    SUDO APT ...

---
+++ fsexportation.sh
@@ -0,0 +1,3 @@
+sudo apt update #Actualiza la lista de paquetes disponibles y sus versiones desde los repositorios.
+sudo apt upgrade #Instala las versiones más recientes de los programas que ya tienes instalados.
+sudo apt install parted #Download and install Parted, a tool for managing hard drive partitions.
