7af421f9c9ffea2074fd032c30ed1ec05b691b53
cayetano-c <cacordovaro@uide.edu.ec>
2026-04-16 16:07:56 +0000

    WIP: ls Explanation

--- script_boot.sh
+++ script_boot.sh
@@ -1,2 +1,4 @@
-cd calculadora/ #ruta relativa
-cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora #ruta absoluta
+cd calculadora/ #relative route
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora #absolute route
+ls -lai #l=list, a=all, i=inode
+ls -l -a -i #the same options but in a separately
