67943d10757607428be0586f704d2333128c4b7d
cayetano-c <cacordovaro@uide.edu.ec>
2026-04-16 16:26:23 +0000

    WIP: Use of the command: stat

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,12 @@ cd calculadora/ #relative route
 cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora #absolute route
 ls -lai #l=list, a=all, i=inode
 ls -l -a -i #the same options but in a separately
+stat . #shows like the ID of the directory
+#File: .
+#Size: 4096            Blocks: 8          IO Block: 4096   directory
+#Device: 7,4     Inode: 1310995     Links: 2
+#Access: (0777/drwxrwxrwx)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+#Access: 2026-04-16 15:59:13.102531427 +0000
+#Modify: 2026-04-16 15:59:13.101531427 +0000
+#Change: 2026-04-16 15:59:13.101531427 +0000
+#Birth: 2026-04-16 15:59:13.101531427 +0000
