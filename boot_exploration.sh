e17e44ae9e748fb6c9e7f69c2fe1b96033792948
cayetano-c <cacordovaro@uide.edu.ec>
2026-04-17 14:24:30 +0000

    WIP: ls uses

---
+++ boot_exploration.sh
@@ -0,0 +1,7 @@
+#List all the directories
+ls -a #Short option
+ls --all #Long option
+#.           Darwin_llave.asc     calculadora                    doc_cifrado_daral.txt   doc_no_cifradoDarwinAl.txt      final_cifrado_firmadoDarwinAl.txt.gpg
+#..          LICENSE              cifrado_firmado_final.txt.gpg  doc_no_cifrado.txt      doc_no_cifradoDarwinAl.txt.gpg  gnupg_script.sh
+#.git        README.md            da_llave_publica.asc           doc_no_cifrado.txt.gpg  doc_no_cifradoDarwinAl.txt.sig  mi_clave_publica.asc
+#.gitignore  boot_exploration.sh  doc_cifrado.txt                doc_no_cifrado.txt.sig  doc_no_cifrado_firmado.txt
