86b79ef40f59f223ed75e73adc5e349c5c3b37b0
cayetano-c <cacordovaro@uide.edu.ec>
2026-04-17 14:28:45 +0000

    WIP: More ls command

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -5,3 +5,31 @@ ls --all #Long option
 #..          LICENSE              cifrado_firmado_final.txt.gpg  doc_no_cifrado.txt      doc_no_cifradoDarwinAl.txt.gpg  gnupg_script.sh
 #.git        README.md            da_llave_publica.asc           doc_no_cifrado.txt.gpg  doc_no_cifradoDarwinAl.txt.sig  mi_clave_publica.asc
 #.gitignore  boot_exploration.sh  doc_cifrado.txt                doc_no_cifrado.txt.sig  doc_no_cifrado_firmado.txt
+
+ls -l -a -h #The command ls -lah is used to list every file in a folder with full details
+ls -l -ah #Short option
+ls -lah #Other short option
+
+#total 128K
+#drwxrwxrwx+ 4 codespace root      4.0K Apr 17 14:15 .
+#drwxr-xrwx+ 5 codespace root      4.0K Apr 17 14:14 ..
+#drwxrwxrwx+ 8 codespace root      4.0K Apr 17 14:24 .git
+#-rw-rw-rw-  1 codespace root      4.6K Apr 17 14:14 .gitignore
+#-rw-rw-rw-  1 codespace root      3.2K Apr 17 14:14 Darwin_llave.asc
+#-rw-rw-rw-  1 codespace root       34K Apr 17 14:14 LICENSE
+#-rw-rw-rw-  1 codespace root        53 Apr 17 14:14 README.md
+#-rw-rw-rw-  1 codespace codespace  721 Apr 17 14:26 boot_exploration.sh
+#drwxrwxrwx+ 2 codespace root      4.0K Apr 17 14:14 calculadora
+#-rw-rw-rw-  1 codespace root      3.2K Apr 17 14:14 da_llave_publica.asc
+#-rw-rw-rw-  1 codespace root       632 Apr 17 14:14 doc_cifrado.txt
+#-rw-rw-rw-  1 codespace root      1.2K Apr 17 14:14 doc_cifrado_daral.txt
+#-rw-rw-rw-  1 codespace root        16 Apr 17 14:14 doc_no_cifrado.txt
+#-rw-rw-rw-  1 codespace root      1.8K Apr 17 14:14 doc_no_cifrado.txt.gpg
+#-rw-rw-rw-  1 codespace root       566 Apr 17 14:14 doc_no_cifrado.txt.sig
+#-rw-rw-rw-  1 codespace root         8 Apr 17 14:14 doc_no_cifradoDarwinAl.txt
+#-rw-rw-rw-  1 codespace root       622 Apr 17 14:14 doc_no_cifradoDarwinAl.txt.gpg
+#-rw-rw-rw-  1 codespace root       566 Apr 17 14:14 doc_no_cifradoDarwinAl.txt.sig
+#-rw-rw-rw-  1 codespace root       898 Apr 17 14:14 doc_no_cifrado_firmado.txt
+#-rw-rw-rw-  1 codespace root      1.2K Apr 17 14:14 final_cifrado_firmadoDarwinAl.txt.gpg
+#-rw-rw-rw-  1 codespace root      1.6K Apr 17 14:14 gnupg_script.sh
+#-rw-rw-rw-  1 codespace root      3.2K Apr 17 14:14 mi_clave_publica.asc
