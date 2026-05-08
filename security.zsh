224f1afee08061a941c4f5179bc364836f6968f6
cayetano-c <cacordovaro@uide.edu.ec>
2026-05-08 14:39:40 +0000

    WIP: SECURITY PART 1

---
+++ security.zsh
@@ -0,0 +1,4 @@
+id #Displays the real and effective user and group IDs.
+groups #Prints the names of the groups the current user belongs to.
+cat /etc/passwd #Displays the contents of the system's user account database.
+cat /etc/passwd | head -10 #Shows only the first 10 entries (lines) of the user account file.
