608085fe1b44c1d354245a4f9dd602824e1900af
cayetano-c <cacordovaro@uide.edu.ec>
2026-05-08 14:44:14 +0000

    WIP: ID of files and directories

--- security.zsh
+++ security.zsh
@@ -2,3 +2,7 @@ id #Displays the real and effective user and group IDs.
 groups #Prints the names of the groups the current user belongs to.
 cat /etc/passwd #Displays the contents of the system's user account database.
 cat /etc/passwd | head -10 #Shows only the first 10 entries (lines) of the user account file.
+
+#My files and directories inherit the GID and UID
+touch test.txt
+ls -la
