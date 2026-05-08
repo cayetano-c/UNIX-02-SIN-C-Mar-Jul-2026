b76881004bbcaacc3ed675f052ba0addeaecccfa
cayetano-c <cacordovaro@uide.edu.ec>
2026-05-08 15:31:23 +0000

    WIP:  Check the system's default settings

--- security.zsh
+++ security.zsh
@@ -19,3 +19,11 @@ groupadd desarroolladores #Create a new standard user group named "desarroollado
 groupadd -g 2000 operaciones #Create a group with a specific Group ID (GID) set to 2000.
 groupadd --system servicios_web #Create a system account group (usually for services, with a GID in the lower reserved range).
 grep -E "desarroolladores|operaciones|servicios_web" /etc/group #Search and filter the group database file using Extended Regular Expressions.
+
+[🐐Cayetano_Córdova] UNIX-02-SIN-C-Mar-Jul-2026 ✓ # grep -E "GID_MIN|GID_MAX|SYS_GID" /etc/login.defs #Search for specific configuration variables in the login definition file to identify the range of Group IDs (GIDs) allowed for regular and system groups.
+GID_MIN                  1000
+GID_MAX                 60000
+#SYS_GID_MIN              101
+#SYS_GID_MAX              999
+SUB_GID_MIN                100000
+SUB_GID_MAX             600100000
