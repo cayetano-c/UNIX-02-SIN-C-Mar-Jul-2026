3043569b19ef5266b0f7b85e61c6c9f037379fd7
cayetano-c <cacordovaro@uide.edu.ec>
2026-05-29 15:08:33 +0000

    WIP: View current groups

--- security2.zsh
+++ security2.zsh
@@ -6,3 +6,10 @@ id -gn
 touch ~/test_grupo_heredado.txt
 # The group is the user's primary group
 ls -la ~/test_grupo_heredado.txt
+# View the current group
+id -gn
+echo "Current group: $(id -gn)"
+
+# Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
