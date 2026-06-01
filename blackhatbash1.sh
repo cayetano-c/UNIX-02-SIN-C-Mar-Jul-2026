9a35cad4e524c5602bc39364a54d20976a276c28
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-01 15:19:15 +0000

    WIP: Debugging

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #!/usr/bin/env bash
+set -x
 bash --version
 env
 echo ${SHELL}
@@ -14,3 +15,12 @@ df --human-readable
 
 #Restricted mode. Disallows certain actions like changing directories (cd) or modifying paths for security.
 #bash -r blackhatbash1.sh
+
+#Executes the script in the current directory.
+#./blackhatbash1.sh
+
+#Grants permission to the owner (u) to execute (x) the file. (Usually, you run this one first so the command above will actually work!)
+chmod u+x blackhatbash1.sh
+
+#bash -n blackhatbash1.sh
+set +x
