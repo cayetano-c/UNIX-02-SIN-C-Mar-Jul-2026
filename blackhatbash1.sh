8d7982b90b58754b13e6defe8c7b452de7e2a854
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-01 15:04:51 +0000

    WIP: The Shebang Line

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+#!/usr/bin/env bash
 bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -ef
+df --human-readable
+bash -x blackhatbash1.sh
+bash -r blackhatbash1.sh
+#blackhatbash1.sh
+#bash blackhatbash1.sh
