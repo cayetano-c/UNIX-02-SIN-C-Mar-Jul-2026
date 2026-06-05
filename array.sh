4443f7e9f830f20b927ef8c82a2dbb3fd0365938
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-05 15:39:53 +0000

    WIP: stdout and stderr texts

--- array.sh
+++ array.sh
@@ -11,3 +11,9 @@ set -x
 echo "${IP_ADDRESSES[0]}"
 
 
+# & ---> Sends a command to the background.
+# &&(and) ---> Used as a logical AND. The second command in the expression will be evaluated only if the first command evaluates to true.
+# ; ---> Used as a list terminator. A command following the terminator will run after the preceding command has finished, regardless of whether it evaluates to true or not.
+# ;; ---> Ends a case statement.
+# | ---> Redirects the output of a command as input to another command.
+# || ---> Used as a logical OR. The second command will run if the first one evaluates to false.
---
+++ blackhatbash5.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#---------------------------
+touch test && touch test123
+#---------------------------
+(ls; ps)
+#---------------------------
+ls; ps; whoami
+#---------------------------
+lzl || echo "el comando lzl falló"
+
+#---------------------------
+# > ---> Redirects stdout to a file
+# >> ---> Redirects stdout to a file by appending it to the existing content
+# &> or >& ---> Redirects stdout and stderr to a file
+# &>> ---> Redirects stdout and stderr to a file by appending them to the existing content
+# <  ---> Redirects input to a command
+# << ---> Called a here document, or heredoc, redirects multiple input lines to a command
+# | ---> Redirects output of a command as input to another command
+
+#---------------------------
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Goodbye!" >> output.txt
+cat output.txt
+
+#---------------------------
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+#---------------------------
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+#---------------------------
+ ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+
