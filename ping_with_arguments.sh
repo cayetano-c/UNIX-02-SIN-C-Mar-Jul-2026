4cea0cb1ffe34c788642eb4f02f15fded74061d1
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-08 15:25:06 +0000

    WIP: Support multipe arguments

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -7,3 +7,10 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping -c 6 "${TARGET}"
+
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
+
+for args in "$@"; do
+    echo "${args}"
+done
