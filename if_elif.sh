6deb0ebe1508953aa30e391ac04754fc1703749d
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-12 14:33:20 +0000

    WIP: Checking Subsequent Conditions

--- if_elif.sh
+++ if_elif.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 USER_INPUT="${1}"
 
-# Check if the user provided an argument
+# We begin with an if statement that checks whether the variable
+USER_INPUT is null
 if [[ -z "${USER_INPUT}" ]]; then
   echo "You must provide an argument!"
   exit 1
 fi
 
-# Check if the argument is of type file or directory
+# This allows us to exit the script early by using exit 1 if we receive no command line arguments from the user. We then begin a second if condition that uses the file test operator to check whether the input is a file
 if [[ -f "${USER_INPUT}" ]]; then
  echo "${USER_INPUT} is a file."
+#Below this condition, we use elif to test whether the argument is a directory 
 elif [[ -d "${USER_INPUT}" ]]; then
   echo "${USER_INPUT} is a directory."
 else
+#This condition won’t be tested unless the file test fails. If neither of these conditions is true, the script responds that the argument is neither a file nor a directory
   echo "${USER_INPUT} is not a file or a directory."
 fi
