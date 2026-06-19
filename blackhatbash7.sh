4d051fad3ee3a26aa43dd8f76afc574f73c19026
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-19 15:14:52 +0000

    WIP: Filtering with grep

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -4,3 +4,17 @@ for index in $(seq 1 10); do
 done
 
 touch example_file1 example_file2 example_file3
+
+grep "35.237.4.214" log.txt
+
+grep "35.237.4.214\|13.66.139.0" log.txt
+
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+
+ps | grep TTY
+
+ps | grep tty
+
+ps | grep -i tty
+
+grep -v "35.237.4.214" log.txt
