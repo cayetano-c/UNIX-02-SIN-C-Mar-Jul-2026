d2db470142e242da1f0e8fc50112f295e1628b2a
cayetano-c <cacordovaro@uide.edu.ec>
2026-06-19 15:51:18 +0000

    WIP: Editing Streams with sed 2

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -42,3 +42,19 @@ awk '{print $7}' log.txt
 
 #Combines both to show only the URLs requested by that specific IP address.
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
+
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+
+sed 's/ //g' log.txt
+
+sed '1d' log.txt
+
+sed '$d' log.txt
+
+sed '5,7d' log.txt
+
+sed -n '2,15 p' log.txt
+
+sed -i '1d' log.txt
